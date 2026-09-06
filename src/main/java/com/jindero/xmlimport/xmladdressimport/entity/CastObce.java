5257217ee76ef1f4e728616b766027e0ca50a229
jinov <jinova95@seznam.cz>
2026-09-06 19:42:09 +0200

    Add CastObce entity

--- src/main/java/com/jindero/xmlimport/xmladdressimport/entity/CastObce.java
+++ src/main/java/com/jindero/xmlimport/xmladdressimport/entity/CastObce.java
@@ -1,4 +1,49 @@
 package com.jindero.xmlimport.xmladdressimport.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "casti_obce")
 public class CastObce {
+
+    @Id
+    @Column(name = "kod")
+    private String kod;
+
+    @Column(name = "nazev")
+    private String nazev;
+
+    @Column(name = "kod_obce")
+    private String kodObce;
+
+    public CastObce() {
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public String getKodObce() {
+        return kodObce;
+    }
+
+    public void setKodObce(String kodObce) {
+        this.kodObce = kodObce;
+    }
 }
